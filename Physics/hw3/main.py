72add3ee8b7127d370260b9e8a05a03d6aa5f5fc
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-10-19 21:34:11 +0500

    added new hw solution

---
+++ Physics/hw3/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+R = float(input("Enter the radius of the circle: "))
+f = float(input("Enter the frequency of the circle: "))
+x0 = float(input("Enter the x-coordinate of the point: "))
+y0 = float(input("Enter the y-coordinate of the point: "))
+
+t = np.linspace(0, 2*np.pi, 1000)
+
+x = R*np.cos(2*np.pi*f*t)
+y = R*np.sin(2*np.pi*f*t)
+
+plt.figure()
+plt.plot(x, y)
+plt.plot(x0, y0, 'ro')
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Trajectory of a point at a rotating circle")
+plt.show()
