28f7442a86a2da22de8ebd420e1ff332bbb0e4c0
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-10-19 21:37:38 +0500

    added new solution

--- Physics/hw3/main.py
+++ Physics/hw3/main.py
@@ -6,15 +6,21 @@ f = float(input("Enter the frequency of the circle: "))
 x0 = float(input("Enter the x-coordinate of the point: "))
 y0 = float(input("Enter the y-coordinate of the point: "))
 
-t = np.linspace(0, 2*np.pi, 1000)
+t = np.linspace(0, 2 * np.pi, 1000)
 
-x = R*np.cos(2*np.pi*f*t)
-y = R*np.sin(2*np.pi*f*t)
+x = R * np.cos(2 * np.pi * f * t)
+y = R * np.sin(2 * np.pi * f * t)
 
-plt.figure()
-plt.plot(x, y)
-plt.plot(x0, y0, 'ro')
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Trajectory of a point at a rotating circle")
-plt.show()
+
+def main():
+    plt.figure()
+    plt.plot(x, y)
+    plt.plot(x0, y0, 'ro')
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Trajectory of a point at a rotating circle")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
